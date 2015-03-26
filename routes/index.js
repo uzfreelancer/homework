a4f2c3542b0b72f1ca0257de80e3670b1f49accd
uzfreelancer <websdemo@gmail.com>
2015-03-26 12:08:52 +0200

    1 commit you can control hero, enemy moves on fixed way

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
  */
 module.exports = function(app){
 
-
+    var steps = 0;
     app.get('/', function (req,res,next){
         //res.send('request received' + req.ip);
         res.status(200).send('Controls for hero /hero//[action]/[x]/[y] <br /> Controls for enemy /enemy/[action]/[x]/[y] <br /> Available actions move or figth');
@@ -13,7 +13,16 @@ module.exports = function(app){
         //var act = req.params.action;
         var x = req.params.x;
         var y = req.params.y;
-        res.status(200).send('Hero moves to (x: '+ x +' , y: '+ y + '}.');
+        var msg = app.get('step')({x:x,y:y});
+        var hero = app.get('hero');
+        var enemy = app.get('enemy');
+        var heroStatus = 'Hero: x = '+ hero.x + ' y = '+ hero.y+ ' health = ' + hero.health;
+        var enemyStatus = 'Enemy: x = '+ enemy.x + ' y = '+ enemy.y+ ' health = ' + enemy.health;
+        steps++;
+        if (hero.health <=0) msg = '<b>Enemy wins!</b>';
+        if (enemy.health <=0) msg = '<b>Hero wins!</b>';
+        res.status(200).send( steps + ': Message: ' + msg + '<br />' + heroStatus + '</br />' + enemyStatus +'<br />');
+
     });
 
     app.get('/hero/fight', function(req,res,next){
--- server.js
+++ server.js
@@ -5,59 +5,52 @@
 var express = require('express');
 var app = express();
 
+var Item = require('./modules/item.js');
+var hero  = new Item(000,000,100,30, 160, 150, false, true, null,0.5, 0.2);
+var enemy = new Item(400,300,200,15, 140, 100, true, false, null,  1, 0.8);
+
+var v2d = require('./modules/v2d.js');
+
+//Массиви шляхів героїв і масив перешкод
+var ways = require('./modules/ways.js');
+var wayhero = ways.way1;
+var wayenemy= ways.way2;
+var barrier= ways.wall;
+wayhero.ind = -1;
+wayenemy.ind = -1;
+
+
+var step = function (point)
+{
+    var msg = "";
+    var hod = hero.range;
+    if (v2d.contains(hero.getPos(),{x:point.x, y:point.y},barrier))
+        msg = 'hero has barrier on way from {'+hero.x+','+hero.y+'} to {'  +wayhero[wayhero.ind].x +',' + wayhero[wayhero.ind].y + '} ';
+    hod = hero.moveTo(point.x, point.y, hod);
+
+    hod = enemy.range;
+    while ( hod > 0 ){
+        wayenemy.ind++;
+        if(wayenemy.ind >= wayenemy.length){ wayenemy.ind = 0};
+        if (v2d.contains(enemy.getPos(),{x:wayenemy[wayenemy.ind].x, y:wayenemy[wayenemy.ind].y},barrier))
+            msg =msg + '/n' +' enemy has barrier on way from {'+enemy.x+','+enemy.y+'} to {'  +wayenemy[wayenemy.ind].x +',' + wayenemy[wayenemy.ind].y + '} ';
+        hod = enemy.moveTo(wayenemy[wayenemy.ind].x, wayenemy[wayenemy.ind].y, hod);
+    };
+
+    var uronk = hero.fight(enemy);
+    enemy.healthTo(uronk);
+    var urons = enemy.fight(hero);
+    hero.healthTo(urons);
+
+    return msg;
+}
+app.set('step',step);
+app.set('hero',hero);
+app.set('enemy',enemy);
+
 require('./routes/index.js')(app);
 
 app.listen(3020,function(){
    console.log('Server starting on port 3020 successfully');
 });
 
-var Item = require('./modules/item.js');
-var komar  = new Item(000,000,100,30, 160, 150, false, true, null,0.5, 0.2);
-var spider = new Item(400,300,200,15, 140, 100, true, false, null,  1, 0.8);
-
-var v2d = require('./modules/v2d.js');
-
-//Массиви шляхів героїв і масив перешкод
-var ways = require('./modules/ways.js');
-var waykomar = ways.way1;
-var wayspider= ways.way2;
-var barrier= ways.wall;
-waykomar.ind = -1;
-wayspider.ind = -1;
-
-console.log('Битва на сервере комара и паука ------------------------------------------------------------------------------------');
-var steps =0;
-while (spider.health >0 && komar.health >0)
-{
-    steps++;
-
-    var hod = spider.range;
-    while ( hod > 0 ){
-        wayspider.ind++;
-        if(wayspider.ind >= wayspider.length){ wayspider.ind = 0};
-        if (v2d.contains(spider.getPos(),{x:wayspider[wayspider.ind].x, y:wayspider[wayspider.ind].y},barrier))
-                console.log('Spider has barrier on way from {'+spider.x+','+spider.y+'} to {'  +wayspider[wayspider.ind].x +',' + wayspider[wayspider.ind].y + '} ');
-        hod = spider.moveTo(wayspider[wayspider.ind].x, wayspider[wayspider.ind].y, hod);
-    }
-    var hod = komar.range;
-    while ( hod > 0 ){
-        waykomar.ind++;
-        if(waykomar.ind >= waykomar.length){ waykomar.ind = 0};
-        if (v2d.contains(komar.getPos(),{x:waykomar[waykomar.ind].x, y:waykomar[waykomar.ind].y},barrier))
-            console.log('Komar has barrier on way from {'+komar.x+','+komar.y+'} to {'  +waykomar[waykomar.ind].x +',' + waykomar[waykomar.ind].y + '} ')
-        hod = komar.moveTo(waykomar[waykomar.ind].x, waykomar[waykomar.ind].y, hod);
-    }
-
-    var uronk = komar.fight(spider);
-    spider.healthTo(uronk);
-    var urons = spider.fight(komar);
-    komar.healthTo(urons);
-
-    //steps++;
-    console.log(steps + ' komar (x,y,health): x:' +komar.x+',y:' +komar.y+',health:'+komar.health + ',uron:' + uronk);
-    console.log(steps + ' spider (x,y,health): x:' +spider.x+ ',y:' +spider.y+',health:'+spider.health + ',uron:'+urons);
-
-};
-var winner =  spider.health > 0 ? "spider" : "komar";
-winner = spider.health ==0 && komar.health ==0  ? "They are both dead. Muha ": winner;
-console.log( winner + " wins at " + steps + " step!")
